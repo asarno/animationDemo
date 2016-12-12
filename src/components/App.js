797a86ef11024d5f88ba376ab48a8b8dd9d91fd8
jaspinall <julia.stefanczyk@gmail.com>
2016-12-12 14:43:04 -0800

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,29 @@
-import React from 'react'
-import { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import { reactiveComponent } from 'omnistream'
 import Box from './Box'
-import DevTools from '../containers/DevTools'
+import { nextTick } from '../actions/actions'
+
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   componentDidMount() {
-    this.animate()
-  }
-
-  animate() {
-    let duration    = 1000 // in ms
-    let fps         = 60 // frames per second
-    let scrollStep  = 1 / ((duration / 1000) * fps)
-    let takeStep    = scrollStep
-    let idx         = 1
-
-  let animationStep = function() {
-    setTimeout(() => {
-      if (this.props.animationProgress < 1) {
-        idx += 1
-        takeStep = scrollStep * idx
-        this.props.nextTick(takeStep)
-        window.requestAnimationFrame(animationStep.bind(this))
-      }
-    }, 1000 / fps);
-  }
-
-    window.requestAnimationFrame(animationStep.bind(this))
+    this.props.dispatchObservableFn(nextTick);
   }
 
   render() {
-
     return (
-      <div className="sky" style={{backgroundColor: this.props.backgroundColor}}>
+      <div className="sky">
         <Box {...this.props}/>
-        <div className="sea" style={{backgroundColor: this.props.seaColor}}>
-          <div className="glare" style={{backgroundColor: this.props.glare}}></div>
+        <div className="sea">
+          <div className="glare"></div>
         </div>
       </div>
     )
   }
 }
 
-export default App
+export default reactiveComponent(App, 'timerState$')
